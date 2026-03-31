0fb4eab3112c8a6f77d708a95bc28e0092d86467
zerriet <zerriet@hotmail.sg>
2026-03-31 16:59:39 +0800

    change

--- src/test/java/com/flexmark/flexMarkProject/controller/InitialControllerIntegrationTest.java
+++ src/test/java/com/flexmark/flexMarkProject/controller/InitialControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -27,6 +28,7 @@ import static org.hamcrest.Matchers.*;
  * Tests the complete HTTP request/response cycle for PDF generation.
  */
 @SpringBootTest
+@ActiveProfiles("poc")
 @DisplayName("InitialController Integration Tests")
 class InitialControllerIntegrationTest {
 
